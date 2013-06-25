de5e828058659718955f4d405752a2afaba34524
Pinak Jain <PJain@theladders.com>
2013-06-25 13:09:39 -0400

    making resume to be a dummy object for now

--- ObjectCalisthenics/src/main/resume/Resume.java
+++ ObjectCalisthenics/src/main/resume/Resume.java
@@ -3,11 +3,8 @@ package main.resume;
 public class Resume
 {
 
-  private final String resume;
-
   public Resume(String resume)
   {
-    if(this.resume == null || this.resume.equals("")) throw new IllegalArgumentException("Resume cannot be null or empty string");
-    this.resume = resume;
+    if(resume == null || resume.equals("")) throw new IllegalArgumentException("Resume cannot be null or empty string");
   }
 }
